b43feecd365d1fdb4e1bfcfe3aafa75b066dc81f
Marko21002 <markgufman21@gmail.com>
2024-04-14 19:19:10 +0200

    space padding delete

--- app/components/space.tsx
+++ app/components/space.tsx
@@ -2,7 +2,7 @@
 
 export default function Space(){
     return(
-        <div className="p-10 mt-6 text-center text-2xl border-2 border-black font-mono font-semibold">
+        <div className=" mt-6 text-center text-2xl border-2 border-black font-mono font-semibold">
         What do we bring to the table?
       </div>
     )
